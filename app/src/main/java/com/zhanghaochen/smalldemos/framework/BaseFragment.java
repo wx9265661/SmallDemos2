f5f1e7bd04d7ab354763eed9ed7e7419e09acb04
K0100014 <K0100014@test.htsc.com.cn>
2019-03-12 15:25:19 +0800

    添加base的fragment

---
+++ app/src/main/java/com/zhanghaochen/smalldemos/framework/BaseFragment.java
@@ -0,0 +1,37 @@
+package com.zhanghaochen.smalldemos.framework;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.Fragment;
+
+import java.lang.ref.WeakReference;
+
+public abstract class BaseFragment extends Fragment {
+
+    protected MyHandler mHandler = new MyHandler(this);
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    protected abstract void handleMessage(Message message);
+
+    protected static class MyHandler extends Handler {
+
+        private final WeakReference<BaseFragment> mFragment;
+
+        public MyHandler(BaseFragment fragment) {
+            mFragment = new WeakReference<>(fragment);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            BaseFragment fragment = mFragment.get();
+            if (fragment != null && fragment.isAdded()) {
+                fragment.handleMessage(msg);
+            }
+            super.handleMessage(msg);
+        }
+    }
+}
